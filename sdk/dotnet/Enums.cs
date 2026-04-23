780a04856f95431c85df302e423d9f39eff82caf
webwarrior-ws <reg@webwarrior.ws>
2026-04-23 11:28:10 +0200

    Revert resource name changes
    Because these changes cause incompatibility with existing
    resources and update workflow fails with error:
    ```
    pulumi:providers:LnVps default_0_0_22  error: Could not automatically download and install resource plugin 'pulumi-resource-LnVps' at version v0.0.22, install the plugin using `pulumi plugin install resource LnVps v0.0.22`: error downloading provider LnVps to file: failed to download plugin: LnVps-0.0.22: 403 HTTP error fetching plugin from https://get.pulumi.com/releases/plugins/pulumi-resource-LnVps-v0.0.22-linux-amd64.tar.gz
    ```

--- sdk/dotnet/Enums.cs
+++ sdk/dotnet/Enums.cs
@@ -5,7 +5,7 @@ using System;
 using System.ComponentModel;
 using Pulumi;
 
-namespace Pulumi.LnVps
+namespace Pulumi.Lnvps
 {
     public enum ImageId
     {
--- sdk/dotnet/Provider.cs
+++ sdk/dotnet/Provider.cs
@@ -7,9 +7,9 @@ using System.Collections.Immutable;
 using System.Threading.Tasks;
 using Pulumi.Serialization;
 
-namespace Pulumi.LnVps
+namespace Pulumi.Lnvps
 {
-    [LnVpsResourceType("pulumi:providers:LnVps")]
+    [LnvpsResourceType("pulumi:providers:lnvps")]
     public partial class Provider : global::Pulumi.ProviderResource
     {
         /// <summary>
@@ -20,7 +20,7 @@ namespace Pulumi.LnVps
         /// <param name="args">The arguments used to populate this resource's properties</param>
         /// <param name="options">A bag of options that control this resource's behavior</param>
         public Provider(string name, ProviderArgs? args = null, CustomResourceOptions? options = null)
-            : base("LnVps", name, args ?? new ProviderArgs(), MakeResourceOptions(options, ""))
+            : base("lnvps", name, args ?? new ProviderArgs(), MakeResourceOptions(options, ""))
         {
         }
 
